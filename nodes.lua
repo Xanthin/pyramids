069e4e421032c9bca7c6935c9cd2f6706ce90e97
Xanthin <Xanthin@users.noreply.github.com>
2014-04-08 23:29:52 +0200

    support for intllib translation mod

--- nodes.lua
+++ nodes.lua
@@ -1,11 +1,20 @@
+-- Boilerplate to support localized strings if intllib mod is installed.
+local S
+if intllib then
+	S = intllib.Getter()
+else
+	S = function(s) return s end
+end
+
+
 local img = {"eye", "men", "sun"}
 
 for i=1,3 do
 	minetest.register_node("pyramids:deco_stone"..i, {
-		description = "Sandstone with "..img[i],
+		description = S("Sandstone with "..img[i]),
 		tiles = {"default_sandstone.png^pyramids_"..img[i]..".png"},
 		is_ground_content = true,
-		groups = {crumbly=2,cracky=3},
+		groups = {crumbly=2,cracky=3,not_in_creative_inventory=1},
 		sounds = default.node_sound_stone_defaults(),
 	})
 end
@@ -25,7 +34,7 @@ trap_on_timer = function (pos, elapsed)
 end
 
 minetest.register_node("pyramids:trap", {
-	description = "Cracked sandstone brick",
+	description = S("Cracked sandstone brick"),
 	tiles = {"default_sandstone_brick.png^pyramids_crack.png"},
 	is_ground_content = true,
 	groups = {crumbly=2,cracky=3},
@@ -39,7 +48,7 @@ minetest.register_node("pyramids:trap", {
 })
 
 minetest.register_node("pyramids:trap_2", {
-	description = "trapstone",
+	description = S("trapstone"),
 	tiles = {"default_sandstone_brick.png^pyramids_crack.png^[transformR90"},
 	is_ground_content = true,
 	groups = {crumbly=2,cracky=3,falling_node=1,not_in_creative_inventory=1},
